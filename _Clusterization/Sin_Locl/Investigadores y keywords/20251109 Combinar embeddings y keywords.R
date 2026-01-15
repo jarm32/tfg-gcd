68880a3e2542833aa53094ae065e0a917207ce17
jarm32 <152169641+jarm32@users.noreply.github.com>
2026-01-15 01:01:27 +0100

    Develop (#30)
    * Clust without locl (#28)
    
    * embeddings wo locl (1)
    
    * preparation new clustering
    
    preparing clustering without localization terms
    
    * datasets to clusterize
    
    creation and concatenation of the datasets which are going to be used to create the new clustering models (the ones without the words that refer to location). In that sense, the new dataset of embeddings for searchers are made by the mean of all the not-location words.
    
    * Clust without locl (#29)
    
    * embeddings wo locl (1)
    
    * preparation new clustering
    
    preparing clustering without localization terms
    
    * datasets to clusterize
    
    creation and concatenation of the datasets which are going to be used to create the new clustering models (the ones without the words that refer to location). In that sense, the new dataset of embeddings for searchers are made by the mean of all the not-location words.
    
    * filtered clustering tests
    
    clustering tests done with the data without the locations words (embeddings)
    
    * update of the memory

--- _Clusterization/Sin_Locl/Investigadores y keywords/20251109 Combinar embeddings y keywords.R
+++ _Clusterization/Sin_Locl/Investigadores y keywords/20251109 Combinar embeddings y keywords.R
@@ -1,4 +1,6 @@
 # Junta los embeddings de las keywords con los investigadores
+# Solo coge las comunes entre ambos archivos --> por si acaso se han quitado las no
+# comunes con antelación
 
 library(readxl)
 library(dplyr)
@@ -6,14 +8,14 @@ library(proxy)
 library(cluster)
 library(factoextra)
 
-setwd("C:/Code/tfg-gcd/_Clusterization/Investigadores y keywords")
+#setwd("C:/Code/tfg-gcd/_Clusterization/Sin_Locl/Investigadores y keywords")
 
-M <- as.data.frame(readxl::read_excel("Matriz_Investigadores_Keywords.xlsx"))
+M <- as.data.frame(readxl::read_excel("Matriz_Investigadores_Keywords_sinlocl.xlsx"))
 rownames(M) <- M[[1]]
 M <- M[,-1]
 M[is.na(M)] <- 0
 
-E <- read.csv("Embeddings_keywords.csv", row.names = 1)
+E <- read.csv("Embeddings_keywords_sinlocl.csv", row.names = 1)
 E <- as.matrix(E)
 
 common_keywords <- intersect(colnames(M), rownames(E))
@@ -26,6 +28,6 @@ X <- as.matrix(M)
 investigadores_vec <- X %*% E   # (investigadores × keywords) × (keywords × dims)
 investigadores_vec <- scale(investigadores_vec)  # opcional
 
-write.csv(investigadores_vec, "Matriz_Investigadores_Embeddings.csv", row.names = TRUE)
+write.csv(investigadores_vec, "Matriz_Investigadores_Embeddings_sinlocl.csv", row.names = TRUE)
 message("Matriz de embeddings de investigadores guardada correctamente.")
 
